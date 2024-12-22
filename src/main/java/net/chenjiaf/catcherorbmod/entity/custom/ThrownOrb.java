be433aec48dff3c8c5b6eeac43134e0014f57bc3
Chen3018 <chenjiafan3018@gmail.com>
2024-12-22 16:07:03 -0500

    Adds storage management for player cages

--- src/main/java/net/chenjiaf/catcherorbmod/entity/custom/ThrownOrb.java
+++ src/main/java/net/chenjiaf/catcherorbmod/entity/custom/ThrownOrb.java
@@ -3,9 +3,11 @@ package net.chenjiaf.catcherorbmod.entity.custom;
 import net.chenjiaf.catcherorbmod.CatcherOrbMod;
 import net.chenjiaf.catcherorbmod.component.ModDataComponentTypes;
 import net.chenjiaf.catcherorbmod.entity.ModEntities;
+import net.chenjiaf.catcherorbmod.item.custom.OrbItem;
 import net.chenjiaf.catcherorbmod.worldgen.dimension.ModDimensions;
 import net.chenjiaf.catcherorbmod.worldgen.portal.ModTeleporter;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.component.DataComponentType;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
@@ -53,10 +55,10 @@ public class ThrownOrb extends ThrowableItemProjectile {
         super.onHitEntity(pResult);
         if (!this.level().isClientSide){
             Entity entityHit = pResult.getEntity();
-            if (! (entityHit instanceof Player)) {
-                ItemEntity orbEntity = new ItemEntity(this.level(), this.getX(), this.getY(), this.getZ(), this.getItem());
-                ItemStack filledOrb = orbEntity.getItem();
+            ItemEntity orbEntity = new ItemEntity(this.level(), this.getX(), this.getY(), this.getZ(), this.getItem());
+            ItemStack filledOrb = orbEntity.getItem();
 
+            if (! (entityHit instanceof Player)) {
                 CompoundTag mobData = new CompoundTag();
                 entityHit.save(mobData);
                 filledOrb.set(ModDataComponentTypes.MOB.get(), mobData);
@@ -65,23 +67,28 @@ public class ThrownOrb extends ThrowableItemProjectile {
                 this.discard();
                 this.level().addFreshEntity(orbEntity);
             } else {
-                ResourceLocation structureLocation = new ResourceLocation(CatcherOrbMod.MODID, "orb_cage");
                 ServerLevel serverLevel = (ServerLevel) this.level();
 
                 if (serverLevel.dimension() != ModDimensions.COCODIM_LEVEL_KEY) {
+                    ResourceLocation structureLocation = new ResourceLocation(CatcherOrbMod.MODID, "orb_cage");
                     StructureTemplate structureTemplate = serverLevel.getStructureManager().get(structureLocation).orElse(null);
 
-                    BlockPos zero = new BlockPos(0, 0, 0);
+                    OrbItem orb = (OrbItem) filledOrb.getItem();
+                    int cageNum = orb.getAvailable();
+                    BlockPos cagePos = orb.getBlockPos(cageNum);
+                    CompoundTag cageTag = new CompoundTag();
+                    cageTag.putInt("cageNum", cageNum);
+                    cageTag.putString("playerId", entityHit.getUUID().toString());
+                    filledOrb.set(ModDataComponentTypes.MOB.get(), cageTag);
+
                     ServerLevel newDimension = serverLevel.getServer().getLevel(ModDimensions.COCODIM_LEVEL_KEY);
                     StructurePlaceSettings settings = new StructurePlaceSettings();
-                    structureTemplate.placeInWorld(newDimension, zero, zero, settings, StructureBlockEntity.createRandom(0), 2);
+                    structureTemplate.placeInWorld(newDimension, cagePos, cagePos, settings, StructureBlockEntity.createRandom(0), 2);
 
-                    entityHit.changeDimension(newDimension, new ModTeleporter(new BlockPos(3, 1, 3), false));
+                    entityHit.changeDimension(newDimension, new ModTeleporter(orb.getSpawnPos(cagePos), false));
                 }
 
-                ItemEntity orbEntity = new ItemEntity(this.level(), this.getX(), this.getY(), this.getZ(), this.getItem());
                 this.level().addFreshEntity(orbEntity);
-
                 this.discard();
             }
         }
--- src/main/java/net/chenjiaf/catcherorbmod/item/custom/OrbItem.java
+++ src/main/java/net/chenjiaf/catcherorbmod/item/custom/OrbItem.java
@@ -1,9 +1,16 @@
 package net.chenjiaf.catcherorbmod.item.custom;
 
+import com.jcraft.jorbis.Block;
+import net.chenjiaf.catcherorbmod.CatcherOrbMod;
 import net.chenjiaf.catcherorbmod.component.ModDataComponentTypes;
 import net.chenjiaf.catcherorbmod.entity.custom.ThrownOrb;
+import net.chenjiaf.catcherorbmod.worldgen.dimension.ModDimensions;
+import net.chenjiaf.catcherorbmod.worldgen.portal.ModTeleporter;
+import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
@@ -17,9 +24,24 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.StructureBlockEntity;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class OrbItem extends Item {
+    private static final int SPACE_INBETWEEN = 5;
+    private static final int NUM_WIDTH = 3;
+    private static final int NUM_HEIGHT = 2;
+    private static final int BLOCK_SIZE = 7;
+
+    // Array list of intervals that are available for storing players.
+    // Intervals are inclusive on both ends and -1 means infinity.
+    private ArrayList<int[]> availableIntervals = new ArrayList<>();
+
     public OrbItem(Properties pProperties) {
         super(pProperties);
     }
@@ -50,15 +72,22 @@ public class OrbItem extends Item {
             ItemStack orb = pContext.getItemInHand();
             CompoundTag entity = orb.get(ModDataComponentTypes.MOB.get());
             if (entity != null) {
-                // Spawn the entity inside the orb
+                if (entity.contains("cageNum")) {
+                    String playerId = entity.getString("playerId");
+                    Player player = level.getPlayerByUUID(UUID.fromString(playerId));
+                    player.changeDimension((ServerLevel) level, new ModTeleporter(pContext.getClickedPos(), false));
 
-                Entity mob = EntityType.loadEntityRecursive(entity, level, (entity1) -> {
-                    entity1.moveTo(pContext.getClickLocation().x(), pContext.getClickLocation().y(), pContext.getClickLocation().z());
-                    return entity1;
-                });
+                    int cageNum = entity.getInt("cageNum");
+                    ServerLevel newDimension = level.getServer().getLevel(ModDimensions.COCODIM_LEVEL_KEY);
+                    freeSpace(cageNum, newDimension);
+                } else {
+                    Entity mob = EntityType.loadEntityRecursive(entity, level, (entity1) -> {
+                        entity1.moveTo(pContext.getClickLocation().x(), pContext.getClickLocation().y(), pContext.getClickLocation().z());
+                        return entity1;
+                    });
 
-                level.addFreshEntity(mob);
-
+                    level.addFreshEntity(mob);
+                }
                 orb.consume(1, pContext.getPlayer());
             }
         }
@@ -75,5 +104,74 @@ public class OrbItem extends Item {
         super.appendHoverText(pStack, pContext, pTooltipComponents, pIsAdvanced);
     }
 
+    public int getAvailable() {
+        if (availableIntervals.isEmpty()) {
+            int[] all = {0, -1};
+            availableIntervals.add(all);
+        }
 
+        int[] first = availableIntervals.removeFirst();
+        if (first[0] != first[1]) {
+            int[] newAvailable = {first[0] + 1, first[1]};
+            availableIntervals.addFirst(newAvailable);
+        }
+
+        return first[0];
+    }
+
+    public void freeSpace(int block, ServerLevel level) {
+        int start = 0;
+        int end = availableIntervals.size() - 1;
+        int mid = 0;
+
+        while (start <= end) {
+            mid = (start + end) / 2;
+            int[] current = availableIntervals.get(mid);
+            if (block < current[0]) {
+                end = mid - 1;
+            } else {
+                if (start == end) {
+                    mid += 1;
+                    break;
+                }
+
+                start = mid + 1;
+            }
+        }
+
+        int[] newInterval = {block, block};
+        if (mid != 0 && availableIntervals.get(mid - 1)[1] == block - 1) {
+            newInterval[0] = availableIntervals.get(mid - 1)[0];
+            availableIntervals.remove(mid - 1);
+            mid -= 1;
+        } else if (availableIntervals.get(mid)[0] == block + 1) {
+            newInterval[1] = availableIntervals.get(mid)[1];
+            availableIntervals.remove(mid);
+        }
+        availableIntervals.add(mid, newInterval);
+
+        ResourceLocation structureLocation = new ResourceLocation(CatcherOrbMod.MODID, "empty_orb_cage");
+        StructureTemplate structureTemplate = level.getStructureManager().get(structureLocation).orElse(null);
+        BlockPos cagePos = getBlockPos(block);
+        StructurePlaceSettings settings = new StructurePlaceSettings();
+        structureTemplate.placeInWorld(level, cagePos, cagePos, settings, StructureBlockEntity.createRandom(0), 2);
+    }
+
+    public BlockPos getBlockPos(int block) {
+        int numStacks = block / NUM_HEIGHT;
+        int xBlock = numStacks % NUM_WIDTH;
+        int xPos = xBlock * (BLOCK_SIZE + SPACE_INBETWEEN);
+
+        int yBlock = block % NUM_HEIGHT;
+        int yPos = yBlock * (BLOCK_SIZE + SPACE_INBETWEEN);
+
+        int zBlock = numStacks / NUM_WIDTH;
+        int zPos = zBlock * (BLOCK_SIZE + SPACE_INBETWEEN);
+
+        return new BlockPos(xPos, yPos, zPos);
+    }
+
+    public BlockPos getSpawnPos(BlockPos cagePos) {
+        return cagePos.above(1).east(BLOCK_SIZE / 2).south(BLOCK_SIZE / 2);
+    }
 }
